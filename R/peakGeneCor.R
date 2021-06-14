31b0632519a65cbc12d0a5bd48bf1b9afc5be5a3
GuandongShang <shangguandong1996@163.com>
2021-06-14 21:06:54 +0800

    add enhancerPromoterCor to link distal enhacer to promoter of gene

--- R/peakGeneCor.R
+++ R/peakGeneCor.R
@@ -142,3 +142,130 @@ peakGeneCor <- function(mmAnno,
 
     return(mmAnno)
 }
+
+
+#' enhancerPromoterCor
+#'
+#' @param peak_GR peak GRange with a column named feature_id representing you peak name
+#' @param Txdb Txdb
+#' @param up_scanPromoter the scan distance which is used to scan nearest promoter
+#' @param down_scanPromoter the scan distance which is used to scan nearest promoter
+#' @param up_scanEnhancer the scan distance which is used to scan feature
+#' @param down_scanEnhacner the scan distance which is used to scan feature
+#' @param peakScoreMt peak count matrix. The rownames are feature_id in peak_GR
+#'
+#' @return mmAnno with Cor, pvalue,padj,qvalue column
+#' @export
+#'
+#' @examples
+enhancerPromoterCor <- function(peak_GR,
+                                Txdb,
+                                up_scanPromoter = 500,
+                                down_scanPromoter = 500,
+                                up_scanEnhancer = 2e4,
+                                down_scanEnhacner = 2e4,
+                                peakScoreMt,
+                                parallel){
+
+    mm_geneScan(peak_GR = peak_GR,
+                Txdb = Txdb,
+                upstream = up_scanPromoter,
+                downstream = down_scanPromoter) -> mm_promoter
+
+    mm_promoter %>%
+        data.frame() %>%
+        dplyr::select(feature_id, gene_id, distanceToTSS) %>%
+        dplyr::mutate(distanceToTSS_abs = abs(distanceToTSS)) %>%
+        dplyr::group_by(gene_id) %>%
+        dplyr::mutate(
+            promoter_feature = feature_id[which.min(distanceToTSS_abs)],
+            peak_gene_pair = paste0(feature_id, ":", gene_id)
+            ) %>%
+        dplyr::ungroup() -> mm_promoter_pair
+
+    pair_info <- mm_promoter_pair %>%
+        dplyr::select(gene_id, promoter_feature) %>%
+        dplyr::distinct() %>%
+        dplyr::mutate(
+            peak_gene_pair = paste0(promoter_feature, ":", gene_id)
+        )
+
+    mm_promoter_pair %>%
+        dplyr::filter(peak_gene_pair %in% pair_info$peak_gene_pair) %>%
+        dplyr::select(gene_id, distanceToTSS, promoter_feature) -> mm_promoter_tidy
+
+
+    mm_geneScan(peak_GR = peak_GR,
+                Txdb = Txdb,
+                upstream = up_scanEnhancer,
+                downstream = down_scanEnhacner) -> mm_scan
+
+    # some gene may not have promoter(no open peak) in here
+    mm_scan <- subset(mm_scan, gene_id %in% mm_promoter_tidy$gene_id)
+
+    mcols(mm_scan) %>%
+        data.frame() %>%
+        dplyr::left_join(mm_promoter_tidy[, c(1, 3)]) -> enhancer_peak_pair
+
+
+    mt_promoter <- peakScoreMt[enhancer_peak_pair$promoter_feature, ]
+    mt_enhancer <- peakScoreMt[enhancer_peak_pair$feature_id, ]
+
+
+    if (parallel) {
+        # print_msgParallel()
+        # though cor(t(mt_gene["gene",]), t(mt_peak["peak,]))
+        # may quickyly produce the whole result
+        # but it will produce a very very huge matrix
+        suppressWarnings(bplapply(seq_len(dim(mt_promoter)[1]), function(index) {
+            cor <- suppressWarnings(cor.test(
+                mt_promoter[index, ],
+                mt_enhancer[index, ]
+            ))
+
+            return(c(cor$estimate, cor$p.value))
+        })) -> cor_result
+    } else {
+        suppressWarnings(lapply(seq_len(dim(mt_promoter)[1]), function(index) {
+            cor <- suppressWarnings(cor.test(
+                mt_promoter[index, ],
+                mt_enhancer[index, ]
+            ))
+
+            return(c(cor$estimate, cor$p.value))
+        })) -> cor_result
+    }
+
+    cor_result <- unlist(cor_result)
+    cor_mt <- matrix(cor_result, ncol = 2, byrow = TRUE)
+
+    mm_scan$promoter_feature <- enhancer_peak_pair$promoter_feature
+    mm_scan$cor <- cor_mt[, 1]
+    mm_scan$pvalue <- cor_mt[, 2]
+    # we do not need result about feature_id's own correlation
+    mm_scan <- subset(mm_scan, feature_id != promoter_feature)
+
+    mm_scan$p_adj <- p.adjust(mm_scan$pvalue)
+
+    # compare with BH, qvalue is more soft
+    qvalue_result <- tryCatch(qvalue::qvalue(
+        p = mm_scan$pvalue,
+        fdr.level = 0.05,
+        pi0.method = "bootstrap"
+    ),
+    error = function(e) NULL
+    )
+
+    if (class(qvalue_result) == "qvalue") {
+        qvalues <- qvalue_result$qvalues
+    } else {
+        qvalues <- NA
+    }
+    mm_scan$qvalue <- qvalues
+
+    metadata(mm_scan)$mm_promoter_tidy <- mm_promoter_tidy
+
+    return(mm_scan)
+
+
+}
